55f236a67112517b03b331f513201989d35387ea
Florian Feichtinger <florian.feichtinger@keytree.co.uk>
2014-12-10 16:06:59 +0100

    converted vanilla quiz to angular

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,7 @@
 angular.module('quizTime', [])
     .controller('QuizController', ['$scope',
         function($scope) {
-            $scope.questions = [{
+            var questions = [{
                 'question': 'Name a programming language that\'s also a gem',
                 'answer': 'Ruby'
             }, {
@@ -16,78 +16,67 @@ angular.module('quizTime', [])
             }, {
                 'question': 'What language do you use to add interactivity to a web page?',
                 'answer': 'JavaScript'
-            }]
-            var questionCount = 0;
-            $scope.reset = function() {
-                $scope.question = $scope.questions[questionCount].question;
+            }];
+
+            var updateQuestion = function() {
+                if (questions[$scope.questionCount]) {
+                    $scope.question = questions[$scope.questionCount].question;
+                }
+            };
+
+            $scope.init = function() {
+                $scope.questionCount = 0;
+                $scope.correct = 0;
                 $scope.answer = '';
-            }
+
+                updateQuestion();
+            };
+
+            $scope.init();
+
+            $scope.answerQuestion = function() {
+                if ($scope.answer.toUpperCase() === questions[$scope.questionCount].answer.toUpperCase()) {
+                    alert('That\'s right!');
+                    $scope.correct += 1;
+                    $scope.answer = '';
+                } else {
+                    alert('That\'s wrong!');
+                }
+
+                $scope.answer = '';
+
+                if ($scope.questionCount < questions.length) {
+                    $scope.questionCount += 1;
+                    updateQuestion();
+                } else {
+                    alert('Quiz completed with ' + correct + ' correct answers!');
+                }
+            };
+
+
+
         }
-    ]);
-/*var container;
+    ])
 
-// quiz begins, no correct answers
-var correct = 0;
-var answer = '';
-
-var questionCount = 0;
-
-// array of questions and corresponding answers
-var questions = [{
-    'question': 'Name a programming language that\'s also a gem',
-    'answer': 'Ruby'
-}, {
-    'question': 'Name a programming language that\'s also a snake',
-    'answer': 'Python'
-}, {
-    'question': 'What language do you use to style web pages?',
-    'answer': 'CSS'
-}, {
-    'question': 'What language do you use to build the structure of web pages?',
-    'answer': 'HTML'
-}, {
-    'question': 'What language do you use to add interactivity to a web page?',
-    'answer': 'JavaScript'
-}];
-
-function init() {
-    // initialise event listeners
-    document.getElementById('reset').addEventListener('click', start);
-    document.getElementById('submitAnswer').addEventListener('click', answerQuestion);
-}
-
-function start() {
-    correct = 0;
-    questionCount = 0;
-    updateTemplate();
-}
-
-function updateTemplate() {
-    document.getElementById('question').innerHTML = questions[questionCount].question;
-    document.getElementById('answerBox').value = '';
-    document.getElementById('answerBox').focus();
-}
-
-function answerQuestion() {
-    answer = document.getElementById('answerBox').value;
-
-    if (answer.toUpperCase() === questions[questionCount].answer.toUpperCase()) {
-        alert('That\'s right!');
-        correct += 1;
-    } else if (answer === '') {
-        document.getElementById('warning').innerHTML = 'Please fill in the answer';
-        return;
-    } else {
-        alert('That\'s wrong!');
-    }
-
-    questionCount += 1;
-
-    if (questionCount < questions.length) {
-        updateTemplate();
-    } else {
-        alert('Quiz completed with ' + correct + ' correct answers!');
-    }
-}
-init();
-*/
+    .directive('focusMe', function($timeout, $parse) {
+        return {
+            //scope: true,   // optionally create a child scope
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.focusMe);
+                scope.$watch(model, function(value) {
+                    console.log('value=', value);
+                    if (value === true) {
+                        $timeout(function() {
+                            element[0].focus();
+                        });
+                    }
+                });
+                // to address @blesh's comment, set attribute value to 'false'
+                // on blur event:
+                element.bind('blur', function() {
+                    console.log('blur');
+                    scope.$apply(model.assign(scope, false));
+                });
+            }
+        };
+    });
