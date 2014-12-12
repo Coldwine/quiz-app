3efd89ab6e8f4b1ed7e62a3e6e5526571a694d9a
Marcel Krause <mgmk@posteo.de>
2014-12-12 12:36:04 +0100

    update formatting

--- js/quiz.js
+++ js/quiz.js
@@ -58,38 +58,38 @@ angular.module('quizTime', [])
         }
     ])
 
-    .directive('ngEnter', function() {
-        return function(scope, element, attrs) {
-            element.bind("keydown keypress", function(event) {
-                if (event.which === 13) {
-                    scope.$apply(function() {
-                        scope.$eval(attrs.ngEnter);
+.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if (event.which === 13) {
+                scope.$apply(function() {
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+})
+
+.directive('focusMe', function($timeout, $parse) {
+    return {
+        //scope: true,   // optionally create a child scope
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.focusMe);
+            scope.$watch(model, function(value) {
+                console.log('value=', value);
+                if (value === true) {
+                    $timeout(function() {
+                        element[0].focus();
                     });
-                    event.preventDefault();
                 }
             });
-        };
-    })
-
-    .directive('focusMe', function($timeout, $parse) {
-        return {
-            //scope: true,   // optionally create a child scope
-            link: function(scope, element, attrs) {
-                var model = $parse(attrs.focusMe);
-                scope.$watch(model, function(value) {
-                    console.log('value=', value);
-                    if (value === true) {
-                        $timeout(function() {
-                            element[0].focus();
-                        });
-                    }
-                });
-                // to address @blesh's comment, set attribute value to 'false'
-                // on blur event:
-                element.bind('blur', function() {
-                    console.log('blur');
-                    scope.$apply(model.assign(scope, false));
-                });
-            }
-        };
-    });
+            // to address @blesh's comment, set attribute value to 'false'
+            // on blur event:
+            element.bind('blur', function() {
+                console.log('blur');
+                scope.$apply(model.assign(scope, false));
+            });
+        }
+    };
+});
